928902476e5c235541a8fc09ace56bfd62e3250a
guidocesa <guidocesa@gmail.com>
2020-09-10 20:45:24 -0300

    Segundo commit

---
+++ TP1/funciones.c
@@ -0,0 +1,79 @@
+void informarResultado(int suma, int resta, float division, int multiplicacion,long int factorialA,long int factorialB, int B)
+{
+    system("cls");
+    printf("El resultado de la suma es %i \n\n", suma);
+    printf("El resultado de la resta es %i \n\n", resta);
+    if(B != 0.0)
+    {
+        printf("El resultado de la division es %f \n\n", division);
+    }
+    else
+    {
+        printf("No se pudo realizar la division porque el divisior es 0 \n\n");
+    }
+    printf("El resultado de la multiplicacion es %i \n\n", multiplicacion);
+    printf("El resultado del factorial A es: %li \n\n", factorialA);
+    printf("El resultado del factorial B es: %li \n\n", factorialB);
+    system("pause");
+}
+int ingresarOperando()
+{
+    int ingreso = 0;
+    system("cls");
+    printf("Ingrese el operando: ");
+    scanf("%i" , &ingreso);
+    return ingreso;
+}
+
+int calcularSuma(int A, int B)
+{
+    return A + B;
+}
+
+int calcularResta(int A, int B)
+{
+    return A - B;
+}
+
+int calcularMultiplicacion(int A, int B)
+{
+    return A*B;
+}
+
+float calcularDivision(int A, int B)
+{
+    if(B != 0)
+    {
+        return (float)A/B;
+    }
+    else
+    {
+        return 0;
+    }
+
+}
+
+int calcularFactorial(int i)
+{
+    if(i > 0)
+    {
+        return (i * calcularFactorial(i - 1));
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int obtenerSeleccion(int A, int B)
+{
+    int seleccion = 0;
+    printf("Por favor elija una opcion y lugo precione enter.\n\n");
+    printf("1.Ingresar 1er operando.  A = %i \n", A);
+    printf("2.Ingresar 2do operando.  B = %i \n", B);
+    printf("3.Colcular todas las operaciones entre los operando.\n");
+    printf("4.Informar los resultados\n");
+    printf("5.Salir\n");
+    scanf("%i" , &seleccion);
+    return seleccion;
+}
---
+++ TP1/funciones.h
@@ -0,0 +1,81 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+
+
+#endif // FUNCIONES_H_INCLUDED
+
+
+/** \brief Escribe los resultados de todas las operaciones
+ *
+ * \param suma int resultado de la suma
+ * \param resta int resultado de la resta
+ * \param division float resultado de la division
+ * \param multiplicacion int resultado de la multiplicacion
+ * \param factorialA long int resultado del primer factorial
+ * \param factorialB long int resultado del segundo factorial
+ * \param B valor del divisor para saber si se pudo realizar la division
+ * \return void
+ *
+ */
+void informarResultado(int suma, int resta, float division, int multiplicacion,long int factorialA,long int factorialB, int B);
+
+/** \brief Obtiene un numero por consola y lo devuelve
+ *
+ * \return int
+ *
+ */
+int ingresarOperando();
+
+/** \brief Calcula la suma entre dos numeros recibidos por parametros
+ *
+ * \param A int primer numero a seumar
+ * \param B int segundo numero a sumar
+ * \return int resultado
+ *
+ */
+int calcularSuma(int A, int B);
+
+/** \brief Calcula la resta entre dos numeros recibidos por parametros
+ *
+ * \param A int numero a ser restado
+ * \param B int numero a restar
+ * \return int resultado
+ *
+ */
+int calcularResta(int A, int B);
+
+/** \brief Devuelve el producto entre dos numeros recibidos por parametro
+ *
+ * \param A int primer numero
+ * \param B int segundo numero
+ * \return int resultado
+ *
+ */
+int calcularMultiplicacion(int A, int B);
+
+/** \brief Devuelve la division entre dos numeros recibidos por parametros
+ *
+ * \param A int Dividendo
+ * \param B int Divisor
+ * \return int Resultado
+ *
+ */
+float calcularDivision(int A, int B);
+
+/** \brief Calcula el factorial de forma recurrente hasta llegar a 0 caso en el que devuelve 1
+ *
+ * \param i int numero a calcular el factorial
+ * \return int resultado
+ *
+ */
+int calcularFactorial(int i);
+
+/** \brief Obtiene por consola una seleccion luego de haber mostrado las opciones por printf
+ *
+ * \param A int Numero ya seleccionado que es mostrado en el menu de opciones pero no manipulado.
+ * \param B int Numero ya seleccionado que es mostrado en el menu de opciones pero no manipulado.
+ * \return int opcion seleccionada
+ *
+ */
+int obtenerSeleccion(int A, int B);
